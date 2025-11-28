8eeff33282dc684f480baba60eef0ba8cbdfddb6
jonorl <39122230+jonorl@users.noreply.github.com>
2025-11-28 14:17:21 +0300

    asdasd

--- lesson120.py
+++ lesson120.py
@@ -1,4 +1,4 @@
-### Convoluted Neural Network
+## Make and evaluate random predictions with best model
 
 import torch
 from torch import nn
@@ -8,21 +8,17 @@ from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 from pathlib import Path 
-from torchmetrics import Accuracy
+from torchmetrics import Accuracy, ConfusionMatrix
 from timeit import default_timer as timer
 from tqdm.auto import tqdm
 import pandas as pd
+import random
+from mlxtend.plotting import plot_confusion_matrix
 
 ### SETUP
 
 MODEL_PATH = Path("models")
 MODEL_PATH.mkdir(parents=True, exist_ok=True) # Create models directory if it doesn't exist
-
-MODEL_NAME_0 = "fashion_mnist_model_0.pth"
-MODEL_SAVE_PATH_0 = MODEL_PATH / MODEL_NAME_0
-
-MODEL_NAME_1 = "fashion_mnist_model_1.pth"
-MODEL_SAVE_PATH_1 = MODEL_PATH / MODEL_NAME_1
 
 MODEL_NAME_2 = "fashion_mnist_model_2.pth"
 MODEL_SAVE_PATH_2 = MODEL_PATH / MODEL_NAME_2
@@ -70,6 +66,7 @@ def eval_model(model: torch.nn.Module,
     return {"model_name": model.__class__.__name__, # This only works if model was created with a class}
             "\nmodel_loss": round(loss.item(), 4),
             "\nmodel_acc": str(round(acc.item() * 100, 2)) + "%"}
+
 
 def train_step(model: torch.nn.Module,
                data_loader: torch.utils.data.DataLoader,
@@ -147,92 +144,35 @@ def test_step(model: torch.nn.Module,
     test_acc /= len(data_loader)
     print(f"Test loss: {test_loss:.5f} | Test acc: {test_acc*100:.2f}%\n")
 
-## Datasets
 
-train_data = datasets.FashionMNIST(
-    root="data", # Where to download data to
-    train=True, # Do we want the training dataset?
-    download=True, # Do we want to download?
-    transform=ToTensor(), # How do we want to transform the data
-    target_transform=None # How do we want to transform the labels/targets
-    )
+def make_predictions(model: torch.nn.Module,
+                     data: list,
+                     device: torch.device = device):
+    pred_probs = []
+    model.to(device)
+    model.eval()
+    with torch.inference_mode():
+        for sample in data:
+            
+            # Prepare the sample (add a batch dimension and pass to target device)
+            sample = torch.unsqueeze(sample, dim=0).to(device)
 
-test_data = datasets.FashionMNIST(
-    root="data", # Where to download data to
-    train=False, # Do we want the training dataset?
-    download=True, # Do we want to download?
-    transform=ToTensor(), # How do we want to transform the data
-    target_transform=None # How do we want to transform the labels/targets
-    )
+            # Forward pass (model outputs raw logits)
+            pred_logit = model(sample)
 
-image, label = train_data[0]
-class_names = train_data.classes
-class_to_idx = train_data.class_to_idx
+            # Get pred prob (logit -> pred prob)
+            pred_prob = torch.softmax(pred_logit.squeeze(), dim=0
+                                      )
+            
+            # Get pred_prob off the GPU for further calculation
+            pred_probs.append(pred_prob.cpu())
 
-# Set Hyperparameters
-EPOCHS = 3
-BATCH_SIZE = 32
-NUM_CLASSES = len(class_names)
-KERNEL_SIZE = 3
-STRIDE = 1
-PADDING = 1
-IMAGE_SIZE = image.shape[1]
+    # Stack the pred_probs to turn list into a tensor
+    return torch.stack(pred_probs)
 
-# Set others
-
-accuracy_fn = Accuracy(task='multiclass', num_classes=NUM_CLASSES).to(device)
-
-# Turn datasets into iterables (batches)
-
-train_dataloader = DataLoader(dataset=train_data,
-                              batch_size=BATCH_SIZE,
-                              shuffle=True)
-
-test_dataloader = DataLoader(dataset=test_data,
-                              batch_size=BATCH_SIZE,
-                              shuffle=False)
-
-train_features_batch, train_labels_batch = next(iter(train_dataloader))
 
 ## Models
 
-class FashionMNISTModelV0(nn.Module):
-    def __init__(self,
-                 input_shape: int,
-                 hidden_units: int,
-                 output_shape: int):
-        super().__init__()
-        self.layer_stack = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(in_features=input_shape,
-                      out_features=hidden_units),
-            nn.Linear(in_features=hidden_units,
-                      out_features=output_shape)
-        )
-
-    def forward(self, x):
-        return self.layer_stack(x)
-    
-class FashionMNISTModelV1(nn.Module):
-    def __init__(self,
-                 input_shape: int,
-                 hidden_units: int,
-                 output_shape: int):
-        super().__init__()
-        self.layer_stack = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(in_features=input_shape,
-                      out_features=hidden_units),
-            nn.ReLU(),
-            nn.Linear(in_features=hidden_units,
-                      out_features=output_shape),
-            nn.ReLU()
-        )
-
-    def forward(self, x:torch.Tensor):
-        return self.layer_stack(x)
-
-## Create a Convolutional Neural Network
 class FashionMNISTModelV2(nn.Module):
     """
     Model architecturae that replicate the TinyVGG
@@ -294,114 +234,67 @@ class FashionMNISTModelV2(nn.Module):
         # print(x.shape)
         x = self.classifier(x)
         return x
+    
+## Datasets
+
+train_data = datasets.FashionMNIST(
+    root="data", # Where to download data to
+    train=True, # Do we want the training dataset?
+    download=True, # Do we want to download?
+    transform=ToTensor(), # How do we want to transform the data
+    target_transform=None # How do we want to transform the labels/targets
+    )
+
+test_data = datasets.FashionMNIST(
+    root="data", # Where to download data to
+    train=False, # Do we want the training dataset?
+    download=True, # Do we want to download?
+    transform=ToTensor(), # How do we want to transform the data
+    target_transform=None # How do we want to transform the labels/targets
+    )
+
+image, label = train_data[0]
+class_names = train_data.classes
+class_to_idx = train_data.class_to_idx
+
+# Set Hyperparameters
+EPOCHS = 3
+BATCH_SIZE = 32
+NUM_CLASSES = len(class_names)
+KERNEL_SIZE = 3
+STRIDE = 1
+PADDING = 1
+IMAGE_SIZE = image.shape[1]
+
+# Set others
+
+accuracy_fn = Accuracy(task='multiclass', num_classes=NUM_CLASSES).to(device)
+
+# Turn datasets into iterables (batches)
+
+train_dataloader = DataLoader(dataset=train_data,
+                              batch_size=BATCH_SIZE,
+                              shuffle=True)
+
+test_dataloader = DataLoader(dataset=test_data,
+                              batch_size=BATCH_SIZE,
+                              shuffle=False)
+
+train_features_batch, train_labels_batch = next(iter(train_dataloader))
 
 ## Loading models
 
 # Initiate model instances
-loaded_model_0 = FashionMNISTModelV0(input_shape=784,
+loaded_model_2 = FashionMNISTModelV2(input_shape=1,
                                       hidden_units=10,
                                       output_shape=len(class_names))
 
-loaded_model_1 = FashionMNISTModelV1(input_shape=784,
-                                     hidden_units=10,
-                                     output_shape=len(class_names)).to(device)
-
-# Load the state dictionary
-
-print(f"Loading model 0 state dict from: {MODEL_SAVE_PATH_0}")
-loaded_model_0.load_state_dict(torch.load(f=MODEL_SAVE_PATH_0))
-
-# Load state dict for model_1
-print(f"Loading model 1 state dict from: {MODEL_SAVE_PATH_1}")
-loaded_model_1.load_state_dict(torch.load(f=MODEL_SAVE_PATH_1))
-
-## Run the models
-
-# Model 0
-
-torch.manual_seed(42)
-
-# Measure time
-train_time_start_on_gpu = timer()
-
-# Create optimisation and eval loop using train_step() and test_step()
-
-for epoch in tqdm(range(EPOCHS)):
-    print(f"Epoch: {epoch}\n-----------")
-    train_step(model=loaded_model_0,
-               data_loader=train_dataloader,
-               loss_fn=nn.CrossEntropyLoss(),
-               optimiser=torch.optim.SGD(params=loaded_model_0.parameters(), # Same as usual
-                            lr=0.1) ,
-               accuracy_fn=accuracy_fn,
-               device='cpu')
-    
-    test_step(model=loaded_model_0,
-              data_loader=test_dataloader,
-              loss_fn=nn.CrossEntropyLoss(),
-              accuracy_fn=accuracy_fn,
-              device='cpu')
-    
-train_time_end_on_gpu = timer()
-total_train_time_model_0 = print_train_time(start=train_time_start_on_gpu,
-                                            end=train_time_end_on_gpu,
-                                            device='cpu')
-print("total train Time:", total_train_time_model_0)
-
-# Get Model_0 results dictionary
-
-model_0_results = eval_model(model=loaded_model_0,
-                             data_loader=test_dataloader,
-                             loss_fn=nn.CrossEntropyLoss(),
-                             accuracy_fn=accuracy_fn,
-                             device='cpu')
-
-# Model 1
-
-torch.manual_seed(42)
-
-# Measure time
-train_time_start_on_gpu = timer()
-
-# Create optimisation and eval loop using train_step() and test_step()
-
-for epoch in tqdm(range(EPOCHS)):
-    print(f"Epoch: {epoch}\n-----------")
-    train_step(model=loaded_model_1,
-               data_loader=train_dataloader,
-               loss_fn=nn.CrossEntropyLoss(),
-               optimiser=torch.optim.SGD(params=loaded_model_1.parameters(), # Same as usual
-                            lr=0.1) ,
-               accuracy_fn=accuracy_fn,
-               device=device)
-    
-    test_step(model=loaded_model_1,
-              data_loader=test_dataloader,
-              loss_fn=nn.CrossEntropyLoss(),
-              accuracy_fn=accuracy_fn,
-              device=device)
-    
-train_time_end_on_gpu = timer()
-total_train_time_model_1 = print_train_time(start=train_time_start_on_gpu,
-                                            end=train_time_end_on_gpu,
-                                            device=device)
-print("total train Time:", total_train_time_model_1)
-
-# Get Model_1 results dictionary
-
-model_1_results = eval_model(model=loaded_model_1,
-                             data_loader=test_dataloader,
-                             loss_fn=nn.CrossEntropyLoss(),
-                             accuracy_fn=accuracy_fn,
-                             device=device)
+# Load state dict for model_2
+print(f"Loading model 1 state dict from: {MODEL_SAVE_PATH_2}")
+loaded_model_2.load_state_dict(torch.load(f=MODEL_SAVE_PATH_2))
+loaded_model_2.to(device)
 
 # Model 2 - CNN
-
-torch.manual_seed(42)
-
-model_2 = FashionMNISTModelV2(input_shape=1,
-                              hidden_units=10,
-                              output_shape=NUM_CLASSES).to(device)
 
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
@@ -413,15 +306,15 @@ train_time_start_model_2 = timer()
 
 for epoch in tqdm(range(EPOCHS)):
     print(f"Epoch: {epoch}\n-----------")
-    train_step(model=model_2,
+    train_step(model=loaded_model_2,
                data_loader=train_dataloader,
                loss_fn=nn.CrossEntropyLoss(),
-               optimiser=torch.optim.SGD(params=model_2.parameters(), # Same as usual
+               optimiser=torch.optim.SGD(params=loaded_model_2.parameters(), # Same as usual
                             lr=0.1) ,
                accuracy_fn=accuracy_fn,
                device=device)
     
-    test_step(model=model_2,
+    test_step(model=loaded_model_2,
               data_loader=test_dataloader,
               loss_fn=nn.CrossEntropyLoss(),
               accuracy_fn=accuracy_fn,
@@ -436,29 +329,94 @@ print("total train Time:", total_train_time_model_2)
 
 # Get Model_2 results dictionary
 
-model_2_results = eval_model(model=model_2,
+model_2_results = eval_model(model=loaded_model_2,
                              data_loader=test_dataloader,
                              loss_fn=nn.CrossEntropyLoss(),
                              accuracy_fn=accuracy_fn,
                              device=device)
 
-# Save model_2 state dict
-print(f"Saving model 2 to: {MODEL_SAVE_PATH_2}")
-torch.save(obj=model_2.state_dict(),
-           f=MODEL_SAVE_PATH_2)
+# random.seed(42)
+test_samples = []
+test_labels = []
+for sample, label in random.sample(list(test_data), k=9): # k is how many random samples we have
+    test_samples.append(sample)
+    test_labels.append(label)
 
-## Compare results
+# View the first sample shape
+test_samples[0].shape
 
-print(model_0_results)
-print(model_1_results)
-print(model_2_results)
+# plt.imshow(test_samples[0].squeeze(), cmap='gray')
+# plt.title(class_names[test_labels[0]])
+# plt.show()
 
-compare_results = pd.DataFrame([model_0_results,
-                               model_1_results,
-                               model_2_results])
+# Make predictions
+pred_probs = make_predictions(model=loaded_model_2,
+                              data=test_samples)
 
-compare_results["training_time"] = [total_train_time_model_0,
-                                    total_train_time_model_1,
-                                    total_train_time_model_2]
+print(pred_probs[:2])
 
-compare_results
+# Convert prediction probs into labels
+pred_classes = pred_probs.argmax(dim=1)
+print(pred_classes)
+
+# Plot predictions
+
+plt.figure(figsize=(9,9))
+nrows = 3
+ncols = 3
+for i, sample in enumerate(test_samples):
+    # Create subplot
+    plt.subplot(nrows, ncols, i+1)
+
+    # Plot the target image
+    plt.imshow(sample.squeeze(), cmap='gray')
+
+    # Find the prediction (in text form e.g. sandal)
+    pred_label = class_names[pred_classes[i]]
+
+    # Get the truth label
+    truth_label = class_names[test_labels[i]]
+
+    # Create a title for the plot
+    title_text = f"Pred: {pred_label} | Truth: {truth_label}"
+    if pred_label == truth_label:
+        plt.title(title_text, fontsize=10, c='g')
+    else:
+        plt.title(title_text, fontsize=10, c='r')
+    plt.axis(False)
+    plt.show()
+
+## Making a confusion matrix for further evaluation
+
+y_preds = []
+loaded_model_2.eval()
+with torch.inference_mode():
+    for X, y in tqdm(test_dataloader, desc="Making predictions..."):
+        # Send the data and targets to device
+        X, y = X.to(device), y.to(device)
+        # Do the forward pass
+        y_logit = loaded_model_2(X)
+        # Turn predictions from logits -> pred probs
+        y_pred = torch.softmax(y_logit.squeeze(), dim=0).argmax(dim=1)
+        # Put predictions on CPU for evaluation
+        y_preds.append(y_pred.cpu())
+
+# Concatenate list of predictions into a tensor
+
+# print(y_preds)
+y_pred_tensor = torch.cat(y_preds)
+print(y_pred_tensor[:10])
+
+# Set up confusion instance and compare prediction to target
+
+confmat = ConfusionMatrix(task="multiclass", num_classes=NUM_CLASSES)
+confmat_tensor = confmat(preds=y_pred_tensor,
+                         target=test_data.targets) # targets = labels
+
+# Plot the confusion matrix
+fig, ax = plot_confusion_matrix(
+    conf_mat=confmat_tensor.numpy(), #matplotlib likes working with numpy
+    class_names=class_names,
+    figsize=(10,7)
+)
+
